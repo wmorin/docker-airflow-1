daa4427628d6e12efae5ecf9d6316505ffe2e98b
Yee-Ting Li <yee379@gmail.com>
2017-10-03 10:54:05 -0700

    extended dags for production

--- dags/pre-processing.py
+++ dags/pre-processing.py
@@ -4,38 +4,87 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
-from airflow.operators import FileGlobSensor 
+
+
+args = {
+    'owner': 'yee',
+    'provide_context': True,
+    'start_date': utils.dates.days_ago(0),
+    'tem': 1,
+    'experiment_id': 'abc123',
+    'source_directory': '/tmp',
+    'source_fileglob': '*.tif',
+    'destination_directory': '/usr/local/airflow/data',
+}
 
 
 
 dag = DAG( 'cryoem_preprocessing',
-    description="Ingestion of CryoEM data",
-    schedule_interval="* * * * *",
-    start_date=utils.dates.days_ago(0)
+    description="Conduct some initial processing to determine efficacy of CryoEM data and upload it to the elogbook",
+    schedule_interval=None,
+    start_date=utils.dates.days_ago(0),
+    default_args=args,
+    max_active_runs=1
 )
 
 
 
-t_wait = FileGlobSensor(task_id='wait_for_new_files', dag=dag,
-    dirpath='/tmp',
-    pattern='*.tif')
-t_stage = DummyOperator(task_id='rename_new_files_for_staging', dag=dag)
-t_parameters = DummyOperator(task_id='determine_tem_parameters', dag=dag)
-t_logbook = DummyOperator(task_id='upload_parameters_to_logbook', dag=dag)
-t_move = DummyOperator(task_id='move_data_file', dag=dag)
-t_motioncorr = DummyOperator(task_id='motioncorr_data_file', dag=dag)
-t_tif2mrc = DummyOperator(task_id='convert_tif_to_mrc', dag=dag)
-t_ctffind = DummyOperator(task_id='ctffind_data', dag=dag)
-t_clean = DummyOperator(task_id='remove_new_files', dag=dag)
+def parseParameters(**kwargs):
+    """read the parameters from the xml"""
+    pass
+t_parameters = PythonOperator(task_id='determine_tem_parameters', dag=dag,
+    python_callable=parseParameters,
+    op_kargs={}
+)
 
 
-t_wait.set_downstream(t_stage)
-t_stage.set_downstream(t_parameters)
-t_parameters.set_downstream(t_logbook)
-t_stage.set_downstream(t_move)
-t_move.set_downstream(t_tif2mrc)
-t_tif2mrc.set_downstream(t_motioncorr)
-t_tif2mrc.set_downstream(t_ctffind)
-t_clean.set_upstream(t_motioncorr)
-t_clean.set_upstream(t_ctffind)
-t_clean.set_upstream(t_logbook)
+def uploadParameters(**kwargs):
+    """Push the parameter key-value pairs to the elogbook"""
+    pass
+t_logbook = PythonOperator(task_id='upload_parameters_to_logbook', dag=dag,
+    python_callable=uploadParameters,
+    op_kwargs={}
+)
+
+
+def tif2mrc(**kwargs):
+    """Convert the tif file to mrc"""
+    pass
+t_tif2mrc = PythonOperator(task_id='convert_tif_to_mrc', dag=dag,
+    python_callable=tif2mrc,
+    op_kwargs={}
+)
+
+
+def motioncorr(**kwargs):
+    """Run motioncorr on the data files and xcom push all of the data"""
+    pass
+t_motioncorr = PythonOperator(task_id='motioncorr', dag=dag,
+    python_callable=motioncorr,
+    op_kwargs={}
+)
+
+t_upload_motioncorr = DummyOperator(task_id='upload_motioncorr_to_logbook', dag=dag)
+
+
+def ctffind(**kwargs):
+    """Run ctffind on the data files and xcom push all of the data"""
+    pass
+t_ctffind = PythonOperator(task_id='ctffind', dag=dag,
+    python_callable=ctffind,
+    op_kwargs={}
+)
+
+
+t_upload_ctffind = DummyOperator(task_id='upload_ctffind_to_logbook', dag=dag)
+
+
+t_clean = DummyOperator(task_id='clean_up', dag=dag)
+
+
+t_parameters >> t_logbook >> t_clean
+
+t_tif2mrc >> t_motioncorr >> t_upload_motioncorr >> t_clean
+
+t_tif2mrc >> t_ctffind >> t_upload_ctffind >> t_clean
+
--- plugins/file_operators.py
+++ plugins/file_operators.py
@@ -1,22 +1,79 @@
-import glob
-
 from airflow.plugins_manager import AirflowPlugin
 from airflow.utils.decorators import apply_defaults
+from airflow.exceptions import AirflowException
 from airflow.operators.sensors import BaseSensorOperator
+from airflow.models import BaseOperator
+from airflow.operators.python_operator import ShortCircuitOperator
+
+import glob
+import shutil
+import os
+import shutil
+from pathlib import Path
+
+import logging
+
+LOG = logging.getLogger(__name__)
 
 class FileGlobSensor(BaseSensorOperator):
     @apply_defaults
-    def __init__(self, dirpath, pattern, *args, **kwargs):
+    def __init__(self, directory, pattern, *args, **kwargs):
         super(FileGlobSensor, self).__init__(*args, **kwargs)
-        self.dirpath = dirpath
+        self.dirpath = directory
         self.globpattern = pattern
-
     def poke(self, context):
         for f in glob.glob( self.dirpath + '/' + self.globpattern ):
-            context['task_instance'].xcom_push('file_path',f)
+            context['task_instance'].xcom_push(key='filepath',value=f)
             return True
+
+def fileGlob(**kwargs):
+    v = '%s/%s' % (kwargs['directory'], kwargs['pattern'])
+    LOG.info('Checking for files with %s' % (v,))
+    for f in glob.glob( v ):
+        LOG.info(" found file %s" % (f,))
+        kwargs['task_instance'].xcom_push(key='filepath',value=f)
+        return f
+    return False
+class FileGlobExistsOperator(ShortCircuitOperator):
+    """ will skip downstream tasks if the file doesn't exist """
+    def __init__(self, directory, pattern, *args, **kwargs):
+        super(FileGlobExistsOperator, self).__init__(python_callable=fileGlob, op_kwargs={'directory': directory, 'pattern': pattern}, *args, **kwargs)
+
+
+
+def ensureDirectoryExists(**kwargs):
+    LOG.info("Checking directory %s" % (kwargs['directory'],))
+    if not os.path.exists(kwargs['directory']):
+        try:
+            os.makedirs(kwargs['directory'])
+        except Exception as e:
+            raise AirflowException('Error creating destination directory: %s' % (e,))
+    return kwargs['directory']
+class EnsureDirectoryExistsOperator(ShortCircuitOperator):
+    """ will create directories specified if it doesn't already exist """
+    def __init__(self,directory,*args,**kwargs):
+        super(EnsureDirectoryExistsOperator,self).__init__(python_callable=ensureDirectoryExists, op_kwargs={'directory': directory}, *args, **kwargs)
+
+
+class FileOperator(BaseOperator):
+    @apply_defaults
+    def __init__(self,source,destination, *args, **kwargs):
+        super(FileOperator, self).__init__(*args,**kwargs)
+        self.src = source
+        self.dst = destination
+    def execute(self, context):
+        self.log.info('Moving file from %s to %s' % (self.src, self.dst))
+        try:
+            shutil.move( self.src, self.dst )
+            return self.dst
+        except Exception as e:
+            raise AirflowException('Error moving file: %s' % e)
+
+
+
+
 
 
 class FilePlugin(AirflowPlugin):
     name = 'file_plugin'
-    operators = [FileGlobSensor]
+    operators = [FileGlobSensor,FileGlobExistsOperator,EnsureDirectoryExistsOperator,FileOperator]
