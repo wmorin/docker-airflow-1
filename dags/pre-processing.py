7a6d419dc5cc0cbf7ad6fd8042b3965ee4c9ed08
Yee-Ting Li <yee379@gmail.com>
2017-10-25 18:01:31 -0700

    reformat dag

--- dags/pre-processing.py
+++ dags/pre-processing.py
@@ -1,6 +1,7 @@
-from datetime import datetime
+
 from airflow import utils
 from airflow import DAG
+
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
@@ -19,72 +20,81 @@ args = {
 
 
 
-dag = DAG( 'cryoem_pre-processing',
-    description="Conduct some initial processing to determine efficacy of CryoEM data and upload it to the elogbook",
-    schedule_interval=None,
-    start_date=utils.dates.days_ago(0),
-    default_args=args,
-    max_active_runs=1
-)
+
+###
+# define the workflow
+###
+with DAG( 'cryoem_pre-processing',
+        description="Conduct some initial processing to determine efficacy of CryoEM data and upload it to the elogbook",
+        schedule_interval=None,
+        start_date=utils.dates.days_ago(0),
+        default_args=args,
+        max_active_runs=1
+    ) as dag:
+
+
+    def parseParameters(**kwargs):
+        """read the parameters from the xml"""
+        pass
+    t_parameters = PythonOperator(task_id='determine_tem_parameters',
+        python_callable=parseParameters,
+        op_kargs={}
+    )
+
+
+    def uploadParameters(**kwargs):
+        """Push the parameter key-value pairs to the elogbook"""
+        pass
+    t_logbook = PythonOperator(task_id='upload_parameters_to_logbook',
+        python_callable=uploadParameters,
+        op_kwargs={}
+    )
+
+
+    def tif2mrc(**kwargs):
+        """Convert the tif file to mrc"""
+        pass
+    t_tif2mrc = PythonOperator(task_id='convert_tif_to_mrc',
+        python_callable=tif2mrc,
+        op_kwargs={}
+    )
+
+
+    def motioncorr(**kwargs):
+        """Run motioncorr on the data files and xcom push all of the data"""
+        pass
+    t_motioncorr = PythonOperator(task_id='motioncorr',
+        python_callable=motioncorr,
+        op_kwargs={}
+    )
+
+    t_upload_motioncorr = DummyOperator(task_id='upload_motioncorr_to_logbook')
+
+
+    def ctffind(**kwargs):
+        """Run ctffind on the data files and xcom push all of the data"""
+        pass
+    t_ctffind = PythonOperator(task_id='ctffind',
+        python_callable=ctffind,
+        op_kwargs={}
+    )
+
+
+    t_upload_ctffind = DummyOperator(task_id='upload_ctffind_to_logbook')
+
+
+    t_clean = DummyOperator(task_id='clean_up')
 
 
 
-def parseParameters(**kwargs):
-    """read the parameters from the xml"""
-    pass
-t_parameters = PythonOperator(task_id='determine_tem_parameters', dag=dag,
-    python_callable=parseParameters,
-    op_kargs={}
-)
+    ###
+    # define pipeline
+    ###
 
 
-def uploadParameters(**kwargs):
-    """Push the parameter key-value pairs to the elogbook"""
-    pass
-t_logbook = PythonOperator(task_id='upload_parameters_to_logbook', dag=dag,
-    python_callable=uploadParameters,
-    op_kwargs={}
-)
+    t_parameters >> t_logbook >> t_clean
 
+    t_tif2mrc >> t_motioncorr >> t_upload_motioncorr >> t_clean
 
-def tif2mrc(**kwargs):
-    """Convert the tif file to mrc"""
-    pass
-t_tif2mrc = PythonOperator(task_id='convert_tif_to_mrc', dag=dag,
-    python_callable=tif2mrc,
-    op_kwargs={}
-)
-
-
-def motioncorr(**kwargs):
-    """Run motioncorr on the data files and xcom push all of the data"""
-    pass
-t_motioncorr = PythonOperator(task_id='motioncorr', dag=dag,
-    python_callable=motioncorr,
-    op_kwargs={}
-)
-
-t_upload_motioncorr = DummyOperator(task_id='upload_motioncorr_to_logbook', dag=dag)
-
-
-def ctffind(**kwargs):
-    """Run ctffind on the data files and xcom push all of the data"""
-    pass
-t_ctffind = PythonOperator(task_id='ctffind', dag=dag,
-    python_callable=ctffind,
-    op_kwargs={}
-)
-
-
-t_upload_ctffind = DummyOperator(task_id='upload_ctffind_to_logbook', dag=dag)
-
-
-t_clean = DummyOperator(task_id='clean_up', dag=dag)
-
-
-t_parameters >> t_logbook >> t_clean
-
-t_tif2mrc >> t_motioncorr >> t_upload_motioncorr >> t_clean
-
-t_tif2mrc >> t_ctffind >> t_upload_ctffind >> t_clean
+    t_tif2mrc >> t_ctffind >> t_upload_ctffind >> t_clean
 
