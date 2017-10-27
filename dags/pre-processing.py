5ef692da053731e7bc784ab0a8b8337ea7958616
Yee-Ting Li <yee379@gmail.com>
2017-10-27 16:28:46 -0700

    add fei xml parsing

--- dags/pre-processing.py
+++ dags/pre-processing.py
@@ -6,6 +6,15 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 
+import sys
+import fileinput
+from pprint import pprint, pformat
+import json
+from collections import defaultdict, MutableMapping
+import xml.etree.ElementTree as ET
+from ast import literal_eval
+from dateutil import parser
+
 
 args = {
     'owner': 'yee',
@@ -21,6 +30,110 @@ args = {
 
 
 
+
+
+
+def conv(v):
+    """ try to parse v to a meaningful type """
+    a = v
+    try:
+        a = literal_eval(v)
+    except:
+        pass
+        if v in ( 'true', 'True' ):
+            a = True
+        elif v in ( 'false', 'False' ):
+            a = False
+        #     a = bool(v)
+    return a
+
+# def conv(v):
+#     return v
+
+# adapted from https://stackoverflow.com/questions/13412496/python-elementtree-module-how-to-ignore-the-namespace-of-xml-files-to-locate-ma
+def etree_to_dict(t):
+    """ convert the etree into a python dict """
+    tag = t.tag
+    if '}' in tag:
+        tag = t.tag.split('}', 1)[1]
+    # LOG.info("TAG: %s" % tag)
+    d = {tag: {} if t.attrib else None}
+    children = list(t)
+    if children:
+        dd = defaultdict(list)
+        for dc in map(etree_to_dict, children):
+            for k, v in dc.items():
+                dd[k].append(v)
+        d = {tag: {k: conv(v[0]) if len(v) == 1 else v
+                     for k, v in dd.items()}}
+        if tag == 'KeyValueOfstringanyType':
+            this_k = None
+            v = None
+            for i,j in dd.items():
+                # LOG.info("  HERE %s -> %s" % (i,j))
+                for a in j:
+                    # LOG.info("    THIS %s" % a)
+                    if i == 'Key':
+                        this_k = a                   
+                    elif i == 'Value' and isinstance(a,dict) and '#text' in a:
+                        # LOG.info("      %s = %s" % (this_k,a['#text']))
+                        v = a['#text']
+                    else:
+                        v = a
+            # LOG.info("+++ %s = %s" % (this_k,v))
+            d = { this_k: conv(v) }
+        
+        # loosing units?
+        elif 'numericValue' in dd:
+            # LOG.info("FOUND numericValue: %s %s" % (tag,dd))
+            d = { tag: dd['numericValue'][0] }
+
+        # remove the units
+        elif tag == 'ReferenceTransformation':
+            # LOG.info("HERE %s" % dd)
+            del d['ReferenceTransformation']['unit']
+
+    if t.attrib:
+        # d[tag].update(('@' + k, v)
+        #                 for k, v in t.attrib.items())
+        for k, v in t.attrib.items():
+            if '}' in k:
+                k = k.split('}', 1)[1]
+            d[tag]['@' + k] = conv(v)
+            # deal with @nil's
+            if k == 'nil':
+                # LOG.info("FOUND %s = %s" % (k, v))
+                d[tag] = conv(v)
+
+    if t.text:
+        text = t.text.strip()
+        if children or t.attrib:
+            if text:
+              d[tag]['#text'] = text
+        else:
+            d[tag] = text
+    return d
+
+
+# from https://stackoverflow.com/questions/6027558/flatten-nested-python-dictionaries-compressing-keys
+def flatten(d, parent_key='', sep='.'):
+    """ flatten the dict d so that we can export as key/value pairs """
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, MutableMapping):
+            items.extend(flatten(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
+
+
+def parseFEIMetaData(**kwargs):
+    """ read the experimental parameters from an FEI xml file """
+    d = etree_to_dict( ET.parse(sys.argv[1]).getroot() )
+    return d
+
+
 ###
 # define the workflow
 ###
@@ -33,20 +146,22 @@ with DAG( 'cryoem_pre-processing',
     ) as dag:
 
 
-    def parseParameters(**kwargs):
-        """read the parameters from the xml"""
-        pass
+    # do i want a file sensor? or do i want to read it in from an external DAG trigger?
+    # pros/cons?
+    # using a file sensor to monitor for new files would allow this is to run indenpendently; however, we would need some state to be recorded so that we may ignore stuff that has already been processed or is currently processing (latter hard)
+
+
     t_parameters = PythonOperator(task_id='determine_tem_parameters',
-        python_callable=parseParameters,
+        python_callable=parseFEIMetaData,
         op_kargs={}
     )
 
 
-    def uploadParameters(**kwargs):
+    def uploadExperimentalParameters(**kwargs):
         """Push the parameter key-value pairs to the elogbook"""
         pass
     t_logbook = PythonOperator(task_id='upload_parameters_to_logbook',
-        python_callable=uploadParameters,
+        python_callable=uploadExperimentalParameters,
         op_kwargs={}
     )
 
