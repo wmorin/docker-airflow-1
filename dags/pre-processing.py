8d10b49bcb962508ee450ee43f76eb06afcaedf3
Yee-Ting Li <yee379@gmail.com>
2017-11-10 02:31:54 -0800

    send epu data to influx

--- dags/pre-processing.py
+++ dags/pre-processing.py
@@ -25,7 +25,6 @@ from pathlib import Path
 import requests
 
 import influxdb
-
 from pprint import pprint, pformat
 
 import json
@@ -161,35 +160,57 @@ def uploadExperimentalParameters2Logbook(ds, **kwargs):
     LOG.warn("data: %s" % (data,))
     raise AirflowSkipException('not yet implemented')
 
-def uploadExperimentalParameters2Influx(ds, measurement='cryoem', **kwargs):
-    """Push the parameter key-value pairs to the elogbook"""
-    d = kwargs['ti'].xcom_pull( task_ids='parse_parameters', key='return_value' )
-    LOG.warn("data: %s" % (data,))
-    dt = parser.parse( d['microscopeData']['acquisition']['acquisitionDateTime'] )
-    # LOG.info("DT %s" % dt)
-    dd = flatten(d, sep='_')
-    context = []
-    data = []
-    for k,v in dd.items():
-        # ignore these entries
-        if k in ( 'microscopeData_acquisition_acquisitionDateTime', 'CustomData_FindFoilHoleCenterResults_@type' ):
-            continue
-        # force context
-        elif k in ( 'microscopeData_instrument_InstrumentID', ):
-            v = '%s' % v
-        # LOG.info("k=%s, v=%s" % (k,v))
-        if isinstance( v, (str,bool) ) or v == None:
-            # LOG.info("  context")
-            vv = "'%s'" % v if isinstance(v,str) and ' ' in v else v
-            context.append("%s=%s"%(k,vv))
-        else:
-            # LOG.info("  data %s" % v)
-            data.append( '%s=%s'%(k,float(v)) )
 
-    # LOG.info("CONTEXT: %s" % (sorted(context),))
-    # LOG.info("DATA: %s" % (sorted(data),))
+class Send2InfluxOperator(PythonOperator):
+    template_fields = ('experiment',)
+    def __init__(self,xcom_task_id,xcom_key,host='localhost',port=8086,user='root',password='root',db='cryoem',measurement='microscope_image',experiment=None,*args,**kwargs):
+        BaseOperator.__init__(self,*args,**kwargs)
+        self.xcom_task_id = xcom_task_id
+        self.xcom_key = xcom_key
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.db = db
+        self.measurement = measurement
+        self.experiment = experiment
+    def execute(self, context,**kwargs):
+        """Push the parameter key-value pairs to the elogbook"""
+        d = context['ti'].xcom_pull( task_ids=self.xcom_task_id, key=self.xcom_key )['MicroscopeImage']
+        dt = parser.parse( d['microscopeData']['acquisition']['acquisitionDateTime'] )
+        dd = flatten(d, sep='_')
+        context = {}
+        data = {} 
+        for k,v in dd.items():
+            # ignore these entries
+            if k in ( 'microscopeData_acquisition_acquisitionDateTime', 'CustomData_FindFoilHoleCenterResults_@type' ):
+                continue
+            # force context
+            elif k in ( 'microscopeData_instrument_InstrumentID', ):
+                v = '%s' % v
+            # LOG.info("k=%s, v=%s" % (k,v))
+            if isinstance( v, (str,bool) ) or v == None:
+                # LOG.info("  context")
+                vv = "'%s'" % v if isinstance(v,str) and ' ' in v else v
+                context[k] = vv
+            else:
+                # LOG.info("  data %s" % v)
+                data[k] = float(v)
 
-    return '%s,%s %s %s' % (measurement,','.join(sorted(context)), ','.join(sorted(data)), dt.strftime('%s'))
+        client = influxdb.InfluxDBClient( self.host, self.port, self.user, self.password, self.db )
+        #LOG.info("DB: %s:%s/%s (%s %s)" % (self.host, self.port, self.db, self.user, self.password) )
+        #LOG.info( '%s @ %s: \n%s\n%s' % (self.measurement,dt.strftime('%s'),pformat(context),pformat(data) ) )
+        client.create_database(self.measurement)
+        if self.experiment:
+           context['experiment'] = self.experiment
+        client.write_points([{
+            "measurement": self.measurement,
+            "tags": context,
+            "fields": data,
+            "time": dt,
+        }])
+        return
+
 
 
 
@@ -215,8 +236,8 @@ def ctffind(context, **kwargs):
 class FeiEpuOperator(PythonOperator):
     """ read the experimental parameters from an FEI xml file """
     template_fields = ('filepath',)
-    def __init__(self,filepath,provide_context=False,*args,**kwargs):
-        BaseOperator.__init__(self,provide_context=provide_context,*args,**kwargs)
+    def __init__(self,filepath,*args,**kwargs):
+        BaseOperator.__init__(self,*args,**kwargs)
         self.filepath = filepath
     def execute(self,context):
         LOG.info('parsing fei epu xml file %s' % (self.filepath,))
@@ -250,10 +271,11 @@ with DAG( 'cryoem_pre-processing',
         python_callable=uploadExperimentalParameters2Logbook,
         op_kwargs={}
     )
-    # upload to influxdb
-    t_param_influx = PythonOperator(task_id='upload_parameters_to_influx',
-        python_callable=uploadExperimentalParameters2Influx,
-        op_kwargs={}
+    t_param_influx = Send2InfluxOperator( task_id='upload_parameters_to_influx',
+        xcom_task_id='parse_parameters',
+        xcom_key='return_value',
+        host='influxdb01.slac.stanford.edu',
+        experiment="{{ dag_run.conf['experiment'] }}",
     )
 
     t_ensure_slack_channel = SlackAPIEnsureChannelOperator( task_id='ensure_slack_channel',
