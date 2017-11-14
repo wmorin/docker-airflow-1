362e178674810b99035a5d7cca42da39a27ef38e
Yee-Ting Li <yee379@gmail.com>
2017-11-14 00:26:33 -0800

    fix fileglobsensor

--- plugins/file_operators.py
+++ plugins/file_operators.py
@@ -26,41 +26,21 @@ import logging
 LOG = logging.getLogger(__name__)
 
 class FileGlobSensor(BaseSensorOperator):
+    template_fields = ( 'filepath', )
     @apply_defaults
-    def __init__(self, directory, pattern, recursive, *args, **kwargs):
-        super(FileGlobSensor, self).__init__(*args, **kwargs)
-        self.dirpath = directory
-        self.globpattern = pattern
+    def __init__(self, filepath, timeout=60, soft_fail=False, poke_interval=5, provide_context=False, recursive=False, *args, **kwargs):
+        super(FileGlobSensor, self).__init__(poke_interval=poke_interval, timeout=timeout, soft_fail=soft_fail, *args, **kwargs)
+        self.filepath = filepath
         self.recursive = recursive
     def poke(self, context):
         files = []
-        os.chdir( self.dirpath )
-        for f in glob.iglob( self.globpattern, recursive=self.recursive ):
+        for f in glob.iglob( self.filepath, recursive=self.recursive ):
             files.append(f) 
         LOG.info('found files: %s' % (files) )
         if len(files):
-            context['task_instance'].xcom_push(key='files',value=files)
-            # file = files.pop(0)
-            # context['task_instance'].xcom_push(key='filepath',value=file)
-            # context['task_instance'].xcom_push(key='directory',value=os.path.dirname(file))
+            context['task_instance'].xcom_push(key='return_value',value=files)
             return True
         return False
-def fileGlob(**kwargs):
-    v = '%s/%s' % (kwargs['directory'], kwargs['pattern'])
-    LOG.info('Checking for files with %s' % (v,))
-    recursive = False
-    if 'recursive' in kwargs:
-        recursive = kwargs['recursive']
-    for f in glob.iglob( v, recursive=recursive ):
-        LOG.info(" found file %s" % (f,))
-        kwargs['task_instance'].xcom_push(key='filepath',value=f)
-        return f
-    return False
-class FileGlobExistsOperator(ShortCircuitOperator):
-    """ will skip downstream tasks if the file doesn't exist """
-    def __init__(self, directory, pattern, recursive, *args, **kwargs):
-        super(FileGlobExistsOperator, self).__init__(python_callable=fileGlob, op_kwargs={'directory': directory, 'pattern': pattern, 'recursive': recursive}, *args, **kwargs)
-
 
 class FileSensor(BaseSensorOperator):
     template_fields = ( 'filepath', )
@@ -71,7 +51,7 @@ class FileSensor(BaseSensorOperator):
     def poke(self, context):
         LOG.info('Waiting for file %s' % (self.filepath,) )
         if os.path.exists( self.filepath ):
-            context['task_instance'].xcom_push(key='file',value=self.filepath)
+            context['task_instance'].xcom_push(key='return_value',value=self.filepath)
             return True
         return False
 
@@ -208,4 +188,4 @@ class RsyncOperator(BaseOperator):
 
 class FilePlugin(AirflowPlugin):
     name = 'file_plugin'
-    operators = [FileGlobSensor,FileGlobExistsOperator,EnsureDirectoryExistsOperator,FileOperator,RsyncOperator,FileSensor]
+    operators = [FileGlobSensor,EnsureDirectoryExistsOperator,FileOperator,RsyncOperator,FileSensor]
