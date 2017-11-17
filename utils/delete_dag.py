6f41d65ee41d1aa712871c07aff9f99092e86c5f
Yee-Ting Li <yee379@gmail.com>
2017-11-17 07:52:04 -0800

    add utility to delete dag from database

---
+++ utils/delete_dag.py
@@ -0,0 +1,10 @@
+import sys
+from airflow.hooks.postgres_hook import PostgresHook
+
+dag_input = sys.argv[1]
+hook=PostgresHook( postgres_conn_id= "airflow_db")
+
+for t in ["xcom", "task_instance", "sla_miss", "log", "job", "dag_run", "dag" ]:
+    sql="delete from {} where dag_id='{}'".format(t, dag_input)
+    hook.run(sql, True)
+
