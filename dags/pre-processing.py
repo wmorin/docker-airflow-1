8d9d7aaba8bf334bc8586044c3c4a9d165b82c6b
Yee-Ting Li <yee379@gmail.com>
2017-11-22 00:23:49 -0800

    remove commented out code

--- dags/pre-processing.py
+++ dags/pre-processing.py
@@ -25,11 +25,6 @@ from datetime import datetime
 # import influxdb
 from pprint import pprint, pformat
 
-# import json
-# from collections import defaultdict, MutableMapping
-# import xml.etree.ElementTree as ET
-# from ast import literal_eval
-# from dateutil import parser
 
 import logging
 LOG = logging.getLogger(__name__)
@@ -42,79 +37,12 @@ args = {
 }
 
 
-#
-# # from https://stackoverflow.com/questions/6027558/flatten-nested-python-dictionaries-compressing-keys
-# def flatten(d, parent_key='', sep='.'):
-#     """ flatten the dict d so that we can export as key/value pairs """
-#     items = []
-#     for k, v in d.items():
-#         new_key = parent_key + sep + k if parent_key else k
-#         if isinstance(v, MutableMapping):
-#             items.extend(flatten(v, new_key, sep=sep).items())
-#         else:
-#             items.append((new_key, v))
-#     return dict(items)
-
-
 
 def uploadExperimentalParameters2Logbook(ds, **kwargs):
     """Push the parameter key-value pairs to the elogbook"""
     data = kwargs['ti'].xcom_pull( task_ids='parse_parameters' )
     LOG.warn("data: %s" % (data,))
     raise AirflowSkipException('not yet implemented')
-
-#
-# class Send2InfluxOperator(PythonOperator):
-#     template_fields = ('experiment',)
-#     def __init__(self,xcom_task_id,xcom_key,host='localhost',port=8086,user='root',password='root',db='cryoem',measurement='microscope_image',experiment=None,*args,**kwargs):
-#         BaseOperator.__init__(self,*args,**kwargs)
-#         self.xcom_task_id = xcom_task_id
-#         self.xcom_key = xcom_key
-#         self.host = host
-#         self.port = port
-#         self.user = user
-#         self.password = password
-#         self.db = db
-#         self.measurement = measurement
-#         self.experiment = experiment
-#     def execute(self, context,**kwargs):
-#         """Push the parameter key-value pairs to the elogbook"""
-#         d = context['ti'].xcom_pull( task_ids=self.xcom_task_id, key=self.xcom_key )['MicroscopeImage']
-#         dt = parser.parse( d['microscopeData']['acquisition']['acquisitionDateTime'] )
-#         dd = flatten(d, sep='_')
-#         context = {}
-#         data = {}
-#         for k,v in dd.items():
-#             # ignore these entries
-#             if k in ( 'microscopeData_acquisition_acquisitionDateTime', 'CustomData_FindFoilHoleCenterResults_@type' ):
-#                 continue
-#             # force context
-#             elif k in ( 'microscopeData_instrument_InstrumentID', ):
-#                 v = '%s' % v
-#             # LOG.info("k=%s, v=%s" % (k,v))
-#             if isinstance( v, (str,bool) ) or v == None:
-#                 # LOG.info("  context")
-#                 vv = "'%s'" % v if isinstance(v,str) and ' ' in v else v
-#                 context[k] = vv
-#             else:
-#                 # LOG.info("  data %s" % v)
-#                 data[k] = float(v)
-#
-#         client = influxdb.InfluxDBClient( self.host, self.port, self.user, self.password, self.db )
-#         #LOG.info("DB: %s:%s/%s (%s %s)" % (self.host, self.port, self.db, self.user, self.password) )
-#         #LOG.info( '%s @ %s: \n%s\n%s' % (self.measurement,dt.strftime('%s'),pformat(context),pformat(data) ) )
-#         client.create_database(self.measurement)
-#         if self.experiment:
-#            context['experiment'] = self.experiment
-#         client.write_points([{
-#             "measurement": self.measurement,
-#             "tags": context,
-#             "fields": data,
-#             "time": dt,
-#         }])
-#         return
-#
-
 
 
 class NotYetImplementedOperator(DummyOperator):
