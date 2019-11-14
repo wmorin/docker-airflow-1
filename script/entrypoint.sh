b5e241ad24a68d2c423e9dcc2c0ddcaa3ebb5d6f
Dawson Mortenson <dawson@agentiq.com>
2019-11-14 15:57:43 -0800

    force it to work

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -8,7 +8,7 @@ account_alias=$(aws iam list-account-aliases | jq --raw-output '.AccountAliases[
 
 aws s3 cp s3://agentiq-demo4-secrets/aiq-airflow-encrypted.env aiq-airflow-encrypted.env
 
-aws kms decrypt --ciphertext-blob fileb://encrypted.env --output text --query Plaintext | base64 -d > decrypted.env
+aws kms decrypt --ciphertext-blob fileb://aiq-airflow-encrypted.env --output text --query Plaintext | base64 -d > decrypted.env
 
 source ./decrypted.env
 
