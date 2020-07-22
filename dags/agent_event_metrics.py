2f9d9829f13ba31e31f7629903d5a98cbed3fee0
Dawson Mortenson <dawson@agentiq.com>
2020-07-22 09:57:42 -0700

    test dag

--- dags/agent_event_metrics.py
+++ dags/agent_event_metrics.py
@@ -38,9 +38,9 @@ default_args = {
     'owner': 'Jaekwan',
     'depends_on_past': False,
     'start_date': datetime(2020, 5, 27),
-    'email': ['swe@agentiq.com'],
+    'email': ['dawson@agentiq.com'],
     'email_on_failure': True,
-    'email_on_retry': False,
+    'email_on_retry': True,
     'retries': 1,
     'retry_delay': timedelta(minutes=5)}
 
