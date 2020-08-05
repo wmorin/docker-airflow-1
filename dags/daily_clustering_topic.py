04f6e05ff49060d3b0565233d106371c03f3a960
Jaekwan Lee <jaekwan@agentiq.com>
2020-08-05 10:00:11 -0700

    Enable email on failure for some dags (#96)

--- dags/daily_clustering_topic.py
+++ dags/daily_clustering_topic.py
@@ -23,7 +23,7 @@ default_args = {
     'depends_on_past': False,
     'start_date': datetime(2020, 6, 1),
     'email': ['swe@agentiq.com'],
-    'email_on_failure': False,
+    'email_on_failure': True,
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5)}
