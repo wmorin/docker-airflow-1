8327a7d5b5b61b16c7a4eae3877e4d129ccfe5a3
Akshay Phadnis <akspha@gmail.com>
2020-08-11 14:31:11 -0700

    ENV from Var and single quots

--- dags/api_exports/s3_path_helper.py
+++ dags/api_exports/s3_path_helper.py
@@ -1,18 +1,19 @@
+from airflow.models import Variable
 from tools.utils.file_util import append_date_to_path
-from tools.config.config import config
 
 from os import path
 
+ENV = Variable.get('ENVIRONMENT')
 
 
 def get_exports_bucket_name():
-    return "exports-api"
+    return 'exports-api'
 
 def invalid_data_dir_name():
-    return "invalid-data"
+    return 'invalid-data'
 
 
 def get_s3_invalid_data_subfolder_path():
-    return append_date_to_path(path.join(config['env'],
+    return append_date_to_path(path.join(ENV,
                                         invalid_data_dir_name())
                                )
