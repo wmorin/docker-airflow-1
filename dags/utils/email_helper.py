d5e9b9df0ac309a76f6a03844963968d106fd0e2
Dawson Mortenson <dawson@agentiq.com>
2020-08-12 13:08:08 -0700

    lint lint

--- dags/utils/email_helper.py
+++ dags/utils/email_helper.py
@@ -1,4 +1,5 @@
 from airflow.utils.email import send_email
+from airflow.models import Variable
 
 def email_notify(context, **kwargs):
     print(context)
@@ -8,16 +9,16 @@ def email_notify(context, **kwargs):
     failed_dag = context.get('task_instance').dag_id
     exec_date = context.get('execution_date')
     log_url = context.get('task_instance').log_url
-    
+
     # Email subject sine
-    subject = f"[{current_env}] Airflow alert: {task_name} Failed".format(**context)
+    subject = f"[{current_env}] Airflow alert: {failed_task} Failed"
     if current_env not in ["demo4", "qae"]:
         subject = "🚨- " + subject
 
     # Relfect correct log url
     if "localhost" in log_url:
         log_url = log_url.replace("http://localhost:8080", "https://aiq-airflow.{current_env}.agentiq.co")
-    
+
     # Email message
     msg = (
         f"🚨 Task Failed 🚨"
@@ -26,7 +27,7 @@ def email_notify(context, **kwargs):
         f"<b>Execution Time</b>: {exec_date}"
         f"<b>Log Url</b>: {log_url}"
         f"<br>"
-        f"There's been an error in the {task_name} job.<br>"
+        f"There's been an error in the {failed_task} job.<br>"
         f"<br>"
         f"params:"
         f"{context.get('params')}"
