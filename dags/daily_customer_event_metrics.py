225b801c98b7e99b390810f3c423c529fe427c5a
Akshay Phadnis <akspha@gmail.com>
2020-09-16 15:46:28 -0700

    python3 and update pytools

--- dags/daily_customer_event_metrics.py
+++ dags/daily_customer_event_metrics.py
@@ -71,10 +71,25 @@ dag.doc_md = __doc__
 # local folder paths = /tmp/customer_events_files/tmp/{env}/%Y-%m-%d
 DESTINATION_PATH = '{{ params.temp_file_path }}/{{ execution_date.format("%Y-%m-%d") }}'
 
+
+
 # First collect the events data from all the resources and write to files
+fetch_id_mappings = BashOperator(
+    task_id='fetch_id_mappings',
+    bash_command='python3 -m tools.analysis.customer_events_metrics'
+                 + ' --start_date="{{ execution_date.format("%Y-%m-%d") }} 00:00:00"'
+                 + ' --end_date="{{ execution_date.format("%Y-%m-%d") }} 23:59:59"'
+                 + ' --populate_uuid_device_mapping'
+                 + ' --timezone="{{ var.value.TIMEZONE }}"',
+    retries=1,
+    env=env,
+    dag=dag)
+
+
+# Second collect the events data from all the resources and write to files
 collection_customer_events = BashOperator(
     task_id='collection_customer_events',
-    bash_command='python -m tools.analysis.customer_events_metrics'
+    bash_command='python3 -m tools.analysis.customer_events_metrics'
                  + ' --start_date="{{ execution_date.format("%Y-%m-%d") }} 00:00:00"'
                  + ' --end_date="{{ execution_date.format("%Y-%m-%d") }} 23:59:59"'
                  + ' --populate_customer_events'
@@ -85,23 +100,10 @@ collection_customer_events = BashOperator(
     dag=dag)
 
 
-# Second collect the events data from all the resources and write to files
-fetch_id_mappings = BashOperator(
-    task_id='fetch_id_mappings',
-    bash_command='python -m tools.analysis.customer_events_metrics'
-                 + ' --start_date="{{ execution_date.format("%Y-%m-%d") }} 00:00:00"'
-                 + ' --end_date="{{ execution_date.format("%Y-%m-%d") }} 23:59:59"'
-                 + ' --populate_uuid_device_mapping'
-                 + ' --timezone="{{ var.value.TIMEZONE }}"',
-    retries=1,
-    env=env,
-    dag=dag)
-
-
 # Third, join the data and verify consistency
 join_data = BashOperator(
     task_id='join_the_data_and_verify_consistency',
-    bash_command='python -m tools.analysis.customer_events_join'
+    bash_command='python3 -m tools.analysis.customer_events_join'
             + f' --stats_data_filename={DESTINATION_PATH}/stats_events.csv'
             + f' --core_db_data_filename={DESTINATION_PATH}/core_db_events.csv',
     retries=1,
@@ -112,7 +114,7 @@ join_data = BashOperator(
 # TODO: FULL_OUTPUT_DIR
 upload_to_db = BashOperator(
     task_id='upload_to_db',
-    bash_command='python -m tools.analysis.customer_events_metrics'
+    bash_command='python3 -m tools.analysis.customer_events_metrics'
                  + ' --upload_to_db'
                  + ' --table_name=customer_events'
                  + f' --filename_to_load={DESTINATION_PATH}/stats_customer_events.csv',
@@ -123,7 +125,7 @@ upload_to_db = BashOperator(
 # Fifth, upload data to active users from customer events
 upload_active_user_to_db = BashOperator(
     task_id='upload_active_user_to_db',
-    bash_command='python -m tools.analysis.customer_events_metrics'
+    bash_command='python3 -m tools.analysis.customer_events_metrics'
                  + ' --end_date="{{ execution_date.format("%Y-%m-%d") }} 23:59:59"'
                  + ' --populate_active_customers',
     retries=1,
