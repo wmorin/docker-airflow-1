a12976a318a8ec44e29da060c5427c3308542d1a
Tomasz <tomasz@fathomhealth.co>
2023-01-30 15:55:45 +0100

    build and push

--- build_arm.sh
+++ build_arm.sh
@@ -14,7 +14,7 @@ function build_and_push() {
 
     pushd $folder
     docker build . -t ${repository}/${image}:${tag} --build-arg FROM_TAG=${tag}
-    # docker push ${repository}/${image}:${tag}
+    docker push ${repository}/${image}:${tag}
     popd
 }
 
