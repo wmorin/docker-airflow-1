462654c1a967815ab9ffa108e41a2ff2009b5513
Tomasz <tomasz@fathomhealth.co>
2023-01-30 15:38:07 +0100

    tweak script

--- build_arm.sh
+++ build_arm.sh
@@ -4,12 +4,19 @@ set -euxo pipefail
 
 repository=us.gcr.io/fathom-containers
 tag=arm
-IMAGES=(airflow_two airflow_two_test)
 
-for image in ${IMAGES[*]}
-do
+IMAGES=(docker-airflow-two docker-airflow-two-test)
+FOLDERS=(airflow_two airflow_two_test)
+
+function build_and_push() {
+    image=$1
+    folder=$2
+
     pushd $image
     docker build . -t ${repository}/${image}:${tag} --build-arg FROM_TAG=${tag}
-    docker push ${repository}/${image}:${tag}
+    # docker push ${repository}/${image}:${tag}
     popd
-done
+}
+
+build_and_push docker-airflow-two airflow_two
+build_and_push docker-airflow-two-test airflow_two_test
