ca70223f969b5a65921c1ee0267d68c42434386b
Tomasz <tomasz@fathomhealth.co>
2023-01-30 15:48:58 +0100

    fix

--- build_arm.sh
+++ build_arm.sh
@@ -12,11 +12,11 @@ function build_and_push() {
     image=$1
     folder=$2
 
-    pushd $image
+    pushd $folder
     docker build . -t ${repository}/${image}:${tag} --build-arg FROM_TAG=${tag}
     # docker push ${repository}/${image}:${tag}
     popd
 }
 
-build_and_push airflow_two docker-airflow-two
-build_and_push airflow_two_test docker-airflow-two-test
+build_and_push docker-airflow-two airflow_two
+build_and_push docker-airflow-two-test airflow_two_test 
